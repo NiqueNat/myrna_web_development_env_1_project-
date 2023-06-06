b516d4300a924996587e07a4a0873dfd62573f68
NiqueVanier <70446500+NiqueNat@users.noreply.github.com>
2023-06-06 18:28:48 -0400

    added links to about and CSS.

--- index.php
+++ index.php
@@ -5,7 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>myrna_web_development_env_1_project</title>
-</head>
+
+    <link rel="stylesheet" href="assets\css\main.css">
+    <link rel="stylesheet" href="about.php">
+
 <body>
       <section class="welcome">
     <div class="container">
