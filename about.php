c95b8c468afed9ff5d6451fb98815b21ea08dc5c
NiqueVanier <70446500+NiqueNat@users.noreply.github.com>
2023-06-06 17:55:09 -0400

    Updated text and added images.

---
+++ about.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>myrna_web_development_env_1_project</title>
+</head>
+<body>
+    <section class="about">
+    <div class="text-box">
+        <h1>About Me</h1>
+        <p>As an online student I have be resilient and determined as a key factors in success. Adult learners face many obstacles and setbacks, such as work and family responsibilities, financial constraints, and personal challenges. However, as a resilient learner, I can overcome these obstacles and persist in my studies. I am able to adapt to changing circumstances and seek out support and resources when needed. 
+        <br>    
+        Determined learners are those who remain focused on their goals and are willing to put in the time and effort required to achieve them. I am not deterred by setbacks or failures and can learn from my mistakes.</p>
+    </div>
+    </section>
+     
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>myrna_web_development_env_1_project</title>
+</head>
+<body>
+      <section class="welcome">
+    <div class="container">
+        <h1>Building the Online Student</h1>
+        <p>Like the facilitator, the online student possesses unique qualities. The online students of today consist primarily of working people who are trying to better their opportunities. This however is changing, as more and more young and older people become aware of the online model. The traditional school will never go away, but the virtual classroom is a significant player in today’s educational community. Corporations are using the online model to train technical professionals while private and public universities redefine the world as their markets. The market for students is expanding rapidly. In general, the online student should possess the following qualities:
+            <br>
+        Be open minded about sharing life, work, and educational experiences as part of the learning process. Introverts as well as extroverts find that the online process requires them to utilize their experiences. This forum for communication eliminates the visual barriers that hinder some individuals in expressing themselves. In addition, the student is given time to reflect on the information before responding. The online environment should be open and friendly.</p>
+    </div>
+    </section>
+
+    <section class="Learning">
+        <div class="courses">
+            <h1>Embrace Online Learning</h1>
+            <p>Embrace that high quality learning can take place without going to a traditional classroom. If the student feels that a traditional classroom is a prerequisite to learning, they may be more comfortable in the traditional classroom. Online is not for everybody. A student that wants to be on a traditional campus attending a traditional classroom is probably not going to be happy online. While the level of social interaction can be very high in the virtual classroom given that many barriers come down in the online format, it is not the same as living in a dorm on a campus. This should be made known. An online student is expected to:
+                <ol>
+                    <li>Participate in the virtual classroom 5-7 days a week</li>
+                    <li>Be able to use the technology properly</li>
+                    <li>Be able to work with others in completing projects</li>
+                    <li>Be able to meet the minimum standards as set forth by the institution</li>
+                    <li>Be able to complete assignments on time</li>
+                    <li>Enjoy communicating in writing</li>
+                </ol>
+            </p>
+    </div>
+    </section>
+
+    <section class="resources">
+        <div class="free-resources">
+            <h1>Top eLearning Resources For Effective Learning Today</h1>
+            <p>What are some of the resources available for online learning? Below are just a few of the options available to you;
+                <ul>
+                    <li>Coursera</li>
+                    <li>edX</li>
+                    <li>YouTube</li>
+                    <li>Lynda</li>
+                    <li>Udemy</li>
+                    <li>Alison</li>
+                    <li>Khan Academy</li>
+                    <li>MIT Open Courseware</li>
+                    <li>Open Culture</li>
+                </ul>
+            </p>
+    </div>
+    </section>
+</body>
+</html>
