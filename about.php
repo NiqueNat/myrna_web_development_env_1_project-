7a5a5d22d8b9e902ae01c9ce42b145a8e5e2f877
NiqueVanier <70446500+NiqueNat@users.noreply.github.com>
2023-06-09 12:52:52 -0400

    added

--- about.php
+++ about.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>myrna_web_development_env_1_project</title>
 
-    
+    <link rel="stylesheet" href="CSS/main.css">
       <style><?php include 'CSS/main.css'; ?></style>
 </head>
 
