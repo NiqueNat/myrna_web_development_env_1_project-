f1be3f6ae1e9d29e1db57fff58687133d9590705
NiqueVanier <70446500+NiqueNat@users.noreply.github.com>
2023-06-09 17:54:32 -0400

    revert style

--- index.php
+++ index.php
@@ -15,38 +15,54 @@
 </head>
 
 <body>
-    <section class="bg-banner">
-        <div class="text-box">
-            <h1>Enjoy The Ocean With Your Family</h1>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-            <a href="#" class="btn">Read More</a>
-        </div>
+
+<header>
+    <a href="index.php">Welcome</a>
+    <a href="about.php">About</a>
+</header>  
+
+      <section class="welcome">
+    <div class="container">
+        <h1>Building the Online Student</h1>
+        <p>Like the facilitator, the online student possesses unique qualities. The online students of today consist primarily of working people who are trying to better their opportunities.</p>
+            <br>
+        Be open minded about sharing life, work, and educational experiences as part of the learning process. Introverts as well as extroverts find that the online process requires them to utilize their experiences. This forum for communication eliminates the visual barriers that hinder some individuals in expressing themselves. In addition, the student is given time to reflect on the information before responding. The online environment should be open and friendly.</p>
+    </div>
     </section>
 
-    <section class="About">
+    <section class="learning">
         <div class="container">
-            <div class="about-flex">
-				<div class="left"></div>
-				<div class="right">
-                    <h2>Welcome To Our Website</h2>
-					<div class="bar"></div>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, quod?</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-
-                    <a href="#" class="btn">Learn More</a>
-                </div>
-			</div>
-		</div> 
-       
+            <h1>Embrace Online Learning</h1>
+            <p>Embrace that high quality learning can take place without going to a traditional classroom. If the student feels that a traditional classroom is a prerequisite to learning, they may be more comfortable in the traditional classroom. Online is not for everybody. A student that wants to be on a traditional campus attending a traditional classroom is probably not going to be happy online. While the level of social interaction can be very high in the virtual classroom given that many barriers come down in the online format, it is not the same as living in a dorm on a campus. This should be made known. An online student is expected to:
+                <ol>
+                    <li>Participate in the virtual classroom 5-7 days a week</li>
+                    <li>Be able to use the technology properly</li>
+                    <li>Be able to work with others in completing projects</li>
+                    <li>Be able to meet the minimum standards as set forth by the institution</li>
+                    <li>Be able to complete assignments on time</li>
+                    <li>Enjoy communicating in writing</li>
+                </ol>
+            </p>
+    </div>
     </section>
 
-    <section class="cta">
-        <div class="container1">
-			<h3>Get In Touch With Us</h3>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-			<a href="#" class="btn">Contact Us</a>
-		</div>
+    <section class="resources">
+        <div class="container">
+            <h1>Top eLearning Resources For Effective Learning Today</h1>
+            <p>What are some of the resources available for online learning? Below are just a few of the options available to you;
+                <ul>
+                    <li>Coursera</li>
+                    <li>edX</li>
+                    <li>YouTube</li>
+                    <li>Lynda</li>
+                    <li>Udemy</li>
+                    <li>Alison</li>
+                    <li>Khan Academy</li>
+                    <li>MIT Open Courseware</li>
+                    <li>Open Culture</li>
+                </ul>
+            </p>
+    </div>
     </section>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </body>
-</html> 
+</html>
