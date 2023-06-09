1d0f63b7c1cf1921ce54dfe89e0b74e8f3b6b8db
NiqueVanier <70446500+NiqueNat@users.noreply.github.com>
2023-06-09 15:38:01 -0400

    updates

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>myrna_web_development_env_1_project</title>
 
-    <link rel="stylesheet" href="assets/css/main.css">
+   <link rel="stylesheet" href="assets/css/main.css">
     
 </head>
 
