bb2131a9f2680be9a1d95335148b99c8d16c05e5
NiqueVanier <70446500+NiqueNat@users.noreply.github.com>
2023-06-09 12:45:51 -0400

    added

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>myrna_web_development_env_1_project</title>
 
-    <link rel="stylesheet" href="CSS/main.css" type="text/css">
+    <link rel="stylesheet" href="CSS/main.css">
 
 </head>
 
