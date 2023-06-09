75922dda6a6c262c0386418c2c0bbdfda55b4841
NiqueVanier <70446500+NiqueNat@users.noreply.github.com>
2023-06-09 12:31:01 -0400

    removed links

--- index.php
+++ index.php
@@ -11,8 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>myrna_web_development_env_1_project</title>
 
-    <link rel="stylesheet" href="CSS/main.css" type="text/css">
-    <link rel="about.php" href='..about.php'>
+    
 
 <body>
 
