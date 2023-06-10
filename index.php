fb2c1e9edd429c54aa7e1130882ae9f154f95de7
NiqueVanier <70446500+NiqueNat@users.noreply.github.com>
2023-06-10 16:31:52 -0400

    undated main.php and css

--- index.php
+++ index.php
@@ -45,42 +45,20 @@
 </section>
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 <section class="learning">
-<div class="container">
-
+<div class="card1">
+    <div class="left">
+        <h1>Be Open</h1>
     <p>Be open minded about sharing life, work, and educational experiences as part of the learning process.
         Introverts as well as extroverts find that the online process requires them to utilize their
         experiences. This forum for communication eliminates the visual barriers that hinder some individuals in
         expressing themselves. In addition, the student is given time to reflect on the information before
         responding. The online environment should be open and friendly.</p>
+    </div>
+    <div class="right"></div>
 
+    <div class="card2">
+    <div class="left">
     <h1>Embrace Online Learning</h1>
     <p>Embrace that high quality learning can take place without going to a traditional classroom. If the
         student feels that a traditional classroom is a prerequisite to learning, they may be more comfortable
@@ -88,7 +66,10 @@
         campus attending a traditional classroom is probably not going to be happy online. While the level of
         social interaction can be very high in the virtual classroom given that many barriers come down in the
         online format, it is not the same as living in a dorm on a campus. This should be made known. An online
-        student is expected to:
+        student is expected to:</p>
+</div>
+
+    <div class="right">
     <ol>
         <li>Participate in the virtual classroom 5-7 days a week</li>
         <li>Be able to use the technology properly</li>
@@ -97,15 +78,14 @@
         <li>Be able to complete assignments on time</li>
         <li>Enjoy communicating in writing</li>
     </ol>
-    </p>
+    </div>
+   
 </div>
 </section>
 
-<section class="resources">
-<div class="container">
+<section class="cta">
+<div class="resources">
     <h1>Top eLearning Resources For Effective Learning Today</h1>
-    <p>What are some of the resources available for online learning? Below are just a few of the options
-        available to you;
     <ul>
         <li>Coursera</li>
         <li>edX</li>
@@ -116,8 +96,8 @@
         <li>Khan Academy</li>
         <li>MIT Open Courseware</li>
         <li>Open Culture</li>
+        <li>Free Code camp</li>
     </ul>
-    </p>
 </div>
 </section>
 </body>
