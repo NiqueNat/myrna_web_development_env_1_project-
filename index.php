149d646dcc01b4fea7bef203c5f9fd2ac07f6544
NiqueVanier <70446500+NiqueNat@users.noreply.github.com>
2023-06-10 15:26:25 -0400

    updated php/css

--- index.php
+++ index.php
@@ -1,87 +1,99 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>myrna_web_development_env_1_project</title>
 
-   <link rel="stylesheet" href="assets/css/main.css">
-    
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>myrna_web_development_env_1_project</title>
+
+<link rel="stylesheet" href="assets/css/main.css">
+
 </head>
 
 <body>
 
 <header class="page-header">
-		<div class="container">
-			<div class="header-flex">
-					<a href="index.html">
-						
-				</div>
+<div class="container">
+    <div class="header-flex">
+        <a href="index.html">
 
-				<div class="andres">
-					<i class="fa-solid fa-bars"></i>
-				</div>
-
-				<nav>
-					<ul class="header-menu">
-						<li>
-							<a href="index.php">Welcome</a>
-						</li>
-						<li>
-							<a href="about.php">About</a>
-						</li>
-					</ul>
-				</nav>
-	</header>
-
-
-
-      <section class="welcome">
-    <div class="container">
-        <h1>Building the Online Student</h1>
-        <p>Like the facilitator, the online student possesses unique qualities. The online students of today consist primarily of working people who are trying to better their opportunities.</p>        
     </div>
-    </section>
 
-    <section class="learning">
-        <div class="container">
-
-        <p>Be open minded about sharing life, work, and educational experiences as part of the learning process. Introverts as well as extroverts find that the online process requires them to utilize their experiences. This forum for communication eliminates the visual barriers that hinder some individuals in expressing themselves. In addition, the student is given time to reflect on the information before responding. The online environment should be open and friendly.</p>
-        
-            <h1>Embrace Online Learning</h1>
-            <p>Embrace that high quality learning can take place without going to a traditional classroom. If the student feels that a traditional classroom is a prerequisite to learning, they may be more comfortable in the traditional classroom. Online is not for everybody. A student that wants to be on a traditional campus attending a traditional classroom is probably not going to be happy online. While the level of social interaction can be very high in the virtual classroom given that many barriers come down in the online format, it is not the same as living in a dorm on a campus. This should be made known. An online student is expected to:
-                <ol>
-                    <li>Participate in the virtual classroom 5-7 days a week</li>
-                    <li>Be able to use the technology properly</li>
-                    <li>Be able to work with others in completing projects</li>
-                    <li>Be able to meet the minimum standards as set forth by the institution</li>
-                    <li>Be able to complete assignments on time</li>
-                    <li>Enjoy communicating in writing</li>
-                </ol>
-            </p>
+    <div class="andres">
+        <i class="fa-solid fa-bars"></i>
     </div>
-    </section>
 
-    <section class="resources">
-        <div class="container">
-            <h1>Top eLearning Resources For Effective Learning Today</h1>
-            <p>What are some of the resources available for online learning? Below are just a few of the options available to you;
-                <ul>
-                    <li>Coursera</li>
-                    <li>edX</li>
-                    <li>YouTube</li>
-                    <li>Lynda</li>
-                    <li>Udemy</li>
-                    <li>Alison</li>
-                    <li>Khan Academy</li>
-                    <li>MIT Open Courseware</li>
-                    <li>Open Culture</li>
-                </ul>
-            </p>
-    </div>
-    </section>
+    <nav>
+        <ul class="header-menu">
+            <li>
+                <a href="index.php">Welcome</a>
+            </li>
+            <li>
+                <a href="about.php">About</a>
+            </li>
+        </ul>
+    </nav>
+</header>
+
+
+
+<section class="welcome">
+<div class="container">
+    <h1>Building the Online Student</h1>
+    <p>Like the facilitator, the online student possesses unique qualities. The online students of today consist
+        primarily of working people who are trying to better their opportunities.</p>
+</div>
+</section>
+
+<section class="learning">
+<div class="container">
+
+    <p>Be open minded about sharing life, work, and educational experiences as part of the learning process.
+        Introverts as well as extroverts find that the online process requires them to utilize their
+        experiences. This forum for communication eliminates the visual barriers that hinder some individuals in
+        expressing themselves. In addition, the student is given time to reflect on the information before
+        responding. The online environment should be open and friendly.</p>
+
+    <h1>Embrace Online Learning</h1>
+    <p>Embrace that high quality learning can take place without going to a traditional classroom. If the
+        student feels that a traditional classroom is a prerequisite to learning, they may be more comfortable
+        in the traditional classroom. Online is not for everybody. A student that wants to be on a traditional
+        campus attending a traditional classroom is probably not going to be happy online. While the level of
+        social interaction can be very high in the virtual classroom given that many barriers come down in the
+        online format, it is not the same as living in a dorm on a campus. This should be made known. An online
+        student is expected to:
+    <ol>
+        <li>Participate in the virtual classroom 5-7 days a week</li>
+        <li>Be able to use the technology properly</li>
+        <li>Be able to work with others in completing projects</li>
+        <li>Be able to meet the minimum standards as set forth by the institution</li>
+        <li>Be able to complete assignments on time</li>
+        <li>Enjoy communicating in writing</li>
+    </ol>
+    </p>
+</div>
+</section>
+
+<section class="resources">
+<div class="container">
+    <h1>Top eLearning Resources For Effective Learning Today</h1>
+    <p>What are some of the resources available for online learning? Below are just a few of the options
+        available to you;
+    <ul>
+        <li>Coursera</li>
+        <li>edX</li>
+        <li>YouTube</li>
+        <li>Lynda</li>
+        <li>Udemy</li>
+        <li>Alison</li>
+        <li>Khan Academy</li>
+        <li>MIT Open Courseware</li>
+        <li>Open Culture</li>
+    </ul>
+    </p>
+</div>
+</section>
 </body>
+
 </html>
