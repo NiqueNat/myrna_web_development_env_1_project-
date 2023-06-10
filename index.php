0a3fd8acf4ac7bf47ab21b7db498cafa96ead2b5
NiqueVanier <70446500+NiqueNat@users.noreply.github.com>
2023-06-10 16:55:30 -0400

    Updated main and css

--- index.php
+++ index.php
@@ -39,77 +39,67 @@
 
 
 <section class="welcome">
-<div class="container">
+<div class="text-box">
     <h1>Building the Online Student</h1>
 </div>
 </section>
 
 
 <section class="learning">
-    <div class="container1">
+    <div class="container">
+        <h2>Learning</h2>
     <div class="learning-flex">
+
 <div class="card1">
-    <div class="left">
+    <div class="info">
         <h1>Be Open</h1>
-    <p>Be open minded about sharing life, work, and educational experiences as part of the learning process.
+         <p>Be open minded about sharing life, work, and educational experiences as part of the learning process.
         Introverts as well as extroverts find that the online process requires them to utilize their
-        experiences. This forum for communication eliminates the visual barriers that hinder some individuals in
-        expressing themselves. In addition, the student is given time to reflect on the information before
-        responding. The online environment should be open and friendly.</p>
+        experiences.</p>
     </div>
-    <div class="right"></div>
-    
-
-
-
-
-
-
-
-
-
-
-
-
+    <div class="image"></div><!--  end of card -->
+   </div>
 
     <div class="card2">
-    <div class="left">
+    <div class="info">
     <h1>Embrace Online Learning</h1>
     <p>Embrace that high quality learning can take place without going to a traditional classroom. If the
         student feels that a traditional classroom is a prerequisite to learning, they may be more comfortable
-        in the traditional classroom. Online is not for everybody. A student that wants to be on a traditional
+        in the traditional classroom. Online is not for everybody. </p>
+        
+        <div class="image"></div><!--  end of card -->
+    </div>
+  <div class="card3">
+    <div class="info">
+    <ol>
+        <li>Participate in the virtual classroom</li>
+        <li>Be able to use the technology properly</li>
+        <li>Be able to work with others</li>
+        <li>Be able to meet the minimum standards</li>
+        <li>Be able to complete assignments on time</li>
+        <li>Enjoy communicating in writing</li>
+    </ol>
+    <div class="image"></div><!--  end of card -->
+    </div>
+    
+    <div class="card3">
+    <div class="info">
+    <p>A student that wants to be on a traditional
         campus attending a traditional classroom is probably not going to be happy online. While the level of
         social interaction can be very high in the virtual classroom given that many barriers come down in the
         online format, it is not the same as living in a dorm on a campus. This should be made known. An online
         student is expected to:</p>
-</div>
-
-    <div class="right">
-    <ol>
-        <li>Participate in the virtual classroom 5-7 days a week</li>
-        <li>Be able to use the technology properly</li>
-        <li>Be able to work with others in completing projects</li>
-        <li>Be able to meet the minimum standards as set forth by the institution</li>
-        <li>Be able to complete assignments on time</li>
-        <li>Enjoy communicating in writing</li>
-    </ol>
+        <div class="image"></div><!--  end of card -->
     </div>
    </div>
-    </div>
+</div>
 </div>
 </section>
 
 
 
-
-
-
-
-
-
-
-
 <section class="cta">
+    <div class="cta-contain">
 <div class="resources">
     <h1>Top eLearning Resources For Effective Learning Today</h1>
     <ul>
@@ -124,8 +114,27 @@
         <li>Open Culture</li>
         <li>Free Code camp</li>
     </ul>
+
+    <a href="">Resources</a>
+</div>
 </div>
 </section>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"
+		integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw=="
+		crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+	<script>
+		$(window).scroll(function () {
+			var scroll = $(window).scrollTop();
+
+			if (scroll >= 100) {
+				$(".page-header").addClass("has-bg");
+			} else {
+				$(".page-header").removeClass("has-bg");
+			}
+		});
+	</script>
 </body>
 
 </html>
